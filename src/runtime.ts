aaa17f5b13e0602180fe57671a8ac460b4f95476
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-08-24 18:25:39 -0400

    refactor: remove unused runtime module

--- src/runtime.ts
+++
@@ -1,7 +0,0 @@
-export { createIsolet } from './create-isolet.js'
-export { defineElement } from './define-element.js'
-export { mountContainer } from './mount-container.js'
-export { injectStyles } from './inject-styles.js'
-export { detectCspNonce } from './detect-csp-nonce.js'
-export type { DefineElementOptions } from './define-element.js'
-export type { IsoletOptions, IsoletInstance, IsolationMode } from './types.js'
